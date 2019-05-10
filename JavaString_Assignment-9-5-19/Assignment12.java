f42c81713641c8eb200f004a490e06c4daef62fe
Nitin Tyagi <nktyagi423@gmail.com>
2019-05-10 09:12:36 +0530

    Add files via upload

---
+++ JavaString_Assignment-9-5-19/Assignment12.java
@@ -0,0 +1,15 @@
+//Write a Java program to test if a given string contains the specified sequence of char values.
+import java.util.*;
+class Assignment12 {
+
+    public static void main(String[] args)
+    {   
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter First String");
+        String str1 = sc.nextLine();
+        System.out.println("Enter First String");
+        String str2 = sc.nextLine();
+        System.out.println("Original String: " + str1);
+        System.out.println("Specified sequence of char values: " + str2);
+    }
+}
